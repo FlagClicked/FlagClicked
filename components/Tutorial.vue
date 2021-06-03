00f2ee1b9b99096be63929d127a882a69466b970
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-03 15:16:29 +0100

    Basic Tutorial Component

---
+++ components/Tutorial.vue
@@ -0,0 +1,20 @@
+<template>
+  <Renderer :content="body" />
+</template>
+<script>
+  export default {
+    props: ["id"],
+    data() {
+      return {
+        author: "",
+        body: ""
+      }
+    },
+    async fetch() {
+      let res = await fetch(`${process.env.backendURL}/tutorials/${this.id}`);
+      let json = await res.json();
+      this.body = json.body;
+      this.author = json.author;
+    }
+  }
+</script>
