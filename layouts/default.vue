9328f3b448479f0738c08a80eefd902491604919
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-05 21:20:49 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="container">
-    <Navbar />
-    <Nuxt class="content" />
+    <Nuxt />
   </div>
 </template>
 
 <script>
-import Navbar from "../components/Navbar";
-
-export default {
-  components: { Navbar },
-};
+export default {}
 </script>
 <style>
 * {
