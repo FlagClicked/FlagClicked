955260e3aba01e465b07f58b1a35279f2bc007cf
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-05 20:30:41 +0000

    format code

--- pages/tutorial/_id.vue
+++ pages/tutorial/_id.vue
@@ -10,23 +10,25 @@
   </div>
 </template>
 <script>
-  export default {
-    head() {
-      title: 'Loading Tutorial'
-    },
-    data() {
-      id: this.$route.params.id
-    },
-    async fetch() {
-        let res = await fetch(`${process.env.backendURL}/tutorial/${id}/title`).then(r => r.text())
-        this.title = `${res} - When Flag Clicked`
-    }
-  }
+export default {
+  head() {
+    title: "Loading Tutorial";
+  },
+  data() {
+    id: this.$route.params.id;
+  },
+  async fetch() {
+    let res = await fetch(
+      `${process.env.backendURL}/tutorial/${id}/title`
+    ).then((r) => r.text());
+    this.title = `${res} - When Flag Clicked`;
+  },
+};
 </script>
 <style>
-  .tutorial-container {
-    width: 100%;
-    height: auto;
-    padding: 22px 22px;
-  }
+.tutorial-container {
+  width: 100%;
+  height: auto;
+  padding: 22px 22px;
+}
 </style>
