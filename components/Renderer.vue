a10ec12e9364f29efe121d3a9dab5a6c97c82ef6
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-05 20:55:22 +0100

    fix renderer

--- components/Renderer.vue
+++ components/Renderer.vue
@@ -5,29 +5,52 @@
 export default {
   props: ["content"],
   data() {
-    return {};
+    return {}
   },
   computed: {
     renderedContent() {
-      const renderCodeBlocks = () => {
-        if (!document.querySelector(".scratchblocks")) {
-          scratchblocks.renderMatching("code.lang-scratchblocks", {
-            style: "scratch3",
-          });
-        }
-        let codeblocks = Array.from(
-          document.querySelectorAll("code:not(.lang-scratchblocks)")
-        );
+      // from https://github.com/jeffalo/ocular/blob/main/components/Render.vue
+      let parser = new DOMParser();
+      let _document = parser.parseFromString(`<html><body>${marked(this.content)}</body></html>`, 'text/html')
+      
+      let options = {
+        style: 'scratch3',
+        inline: false,
+        languages: ['en'],
+        
+        read: scratchblocks.read,
+        parse: scratchblocks.parse,
+        render: scratchblocks.render
+      }
+      
+      
+      let results = Array.from(_document.querySelectorAll('code.lang-scratchblocks'))
+      
+      results.forEach(scratchblocks => {
+        let code = options.read(scratchblocks, options)
+        let parsed = options.parse(code, options)
+        let svg = options.render(parsed, options)
+        
+        let container = _document.createElement('div');
+        container.classList.add('scratchblocks');
+        
+        container.appendChild(svg);
+        
+        scratchblocks.innerHTML = '';
+        
+        scratchblocks.appendChild(container)
+      })
+      
+      let codeblocks = Array.from(document.querySelectorAll('code:not(.lang-scratchblocks)'))
+      
+      codeblocks.forEach(el => {
+        el.setAttribute('data-language', el.classList[0]?.slice(5));
+        Rainbow.color(el.parentNode)
+      })
 
-        codeblocks.forEach((el) => {
-          el.setAttribute("data-language", el.classList[0]?.slice(5));
-          Rainbow.color(el.parentNode);
-        });
-      };
-      renderCodeBlocks();
-
-      return marked(this.content, { sanitize: true });
-    },
+      return _document.querySelector('body').innerHTML
+    }
   },
-};
+  ssr: false
+}
 </script>
