4f1ce5683c60adccd915d8c5b5ff3c9d8747fee9
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-05 21:30:21 +0100

    Create _id.vue

---
+++ pages/tutorial/_id.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <Navbar />
+    <div class="margined">
+      <div class="tutorial-container">
+        <Tutorial :id="id" />
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+<script>
+  export default {
+    head() {
+      title: 'Loading Tutorial'
+    },
+    data() {
+      id: this.$route.params.id
+    },
+    async fetch() {
+        let res = await fetch(`${process.env.backendURL}/tutorial/${id}/title`).then(r => r.text())
+        this.title = `${res} - When Flag Clicked`
+    }
+  }
+</script>
+<style>
+  .tutorial-container {
+    width: 100%;
+    height: auto;
+    padding: 22px 22px;
+  }
+</style>
