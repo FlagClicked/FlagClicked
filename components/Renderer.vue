aac82dd30998978bddf7a9ef5a9732bd2e551acd
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-05 20:35:09 +0100

    Update Renderer.vue

--- components/Renderer.vue
+++ components/Renderer.vue
@@ -9,7 +9,6 @@ export default {
   },
   computed: {
     renderedContent() {
-      let requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame
       const renderCodeBlocks = () => {
         if (!document.querySelector(".scratchblocks")) {
           scratchblocks.renderMatching("code.lang-scratchblocks", {
@@ -23,14 +22,9 @@ export default {
           Rainbow.color(el.parentNode);
         })
       };
-      requestAnimationFrame(renderCodeBlocks);
+      renderCodeBlocks()
 
-
-      function addEmbeds(html) {
-        return html.replace(/\$<a href="(.*?)">(.*?)<\/a>/g, (match, url, title) => `<iframe width="560" height="315" src="https://www.youtube.com/embed/${/(\/youtube\/|\?v=)(.*?)($|\/|&|#)/.exec(url[2]).replace(/"/g, '\\"')}" title="${title.replace(/"/g,'\\"')}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`).replace(/<a href="(\d+)">(project|turbowarp)<\/a>/g,(match, id, website) =>`<iframe src="https://${website === "turbowarp"? "turbowarp.org": "scratch.mit.edu/projects"}/${id}/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>`;)
-      };
-
-      return addEmbeds(marked(this.content, { sanitize: true }))
+      return (marked(this.content, { sanitize: true }))
     }
   }
 }
