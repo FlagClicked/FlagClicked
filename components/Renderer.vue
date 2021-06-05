71c2c53512f6126281ed25a4f507183c4162ee30
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-05 19:35:32 +0000

    format code

--- components/Renderer.vue
+++ components/Renderer.vue
@@ -5,27 +5,29 @@
 export default {
   props: ["content"],
   data() {
-    return {}
+    return {};
   },
   computed: {
     renderedContent() {
       const renderCodeBlocks = () => {
         if (!document.querySelector(".scratchblocks")) {
           scratchblocks.renderMatching("code.lang-scratchblocks", {
-            style: "scratch3"
-          })
+            style: "scratch3",
+          });
         }
-        let codeblocks = Array.from(document.querySelectorAll("code:not(.lang-scratchblocks)"));
+        let codeblocks = Array.from(
+          document.querySelectorAll("code:not(.lang-scratchblocks)")
+        );
 
         codeblocks.forEach((el) => {
           el.setAttribute("data-language", el.classList[0]?.slice(5));
           Rainbow.color(el.parentNode);
-        })
+        });
       };
-      renderCodeBlocks()
+      renderCodeBlocks();
 
-      return (marked(this.content, { sanitize: true }))
-    }
-  }
-}
+      return marked(this.content, { sanitize: true });
+    },
+  },
+};
 </script>
