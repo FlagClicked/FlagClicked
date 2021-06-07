213acff5a49fb60e025df4807444e0b38c1629ab
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-07 19:06:32 +0100

    aaaa

--- pages/login.vue
+++ pages/login.vue
@@ -31,6 +31,7 @@ export default {
     }
     if (this.$route.query.token) {
       cookies.set("auth", this.$route.query.token);
+      this.$router.push({ path: "/" })
       return;
     }
     this.link = `${process.env.backendURL}/auth/begin`;
