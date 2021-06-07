3d8c4d7e40f9e7c9e41f42eee135c903d7092c08
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-07 18:22:05 +0000

    format code

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,15 +1,15 @@
-const cookie = process.server ? require('cookie') : require('js-cookie')
+const cookie = process.server ? require("cookie") : require("js-cookie");
 
-export default async function({ redirect, req, store }) {
-  let token = null
-  
+export default async function ({ redirect, req, store }) {
+  let token = null;
+
   if (process.server) {
     if (req.headers.cookie) {
-      let token = cookie.parse(req.headers.cookie)
+      let token = cookie.parse(req.headers.cookie);
     }
   } else {
-    token = cookie.get('auth')
+    token = cookie.get("auth");
   }
-  
+
   // TODO: more stuff here
 }
