b4e5910c5d2102da0bc5667991c79c8c2f16cc60
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-07 17:57:33 +0000

    format code

--- pages/tutorials/new.vue
+++ pages/tutorials/new.vue
@@ -3,7 +3,12 @@
     <Navbar />
     <div class="margined">
       <div class="tutorial-editor">
-        <input type="text" placeholder="Title" @input="updateTitle" :value="title" />
+        <input
+          type="text"
+          placeholder="Title"
+          @input="updateTitle"
+          :value="title"
+        />
         <textarea :value="input" @input="update"></textarea>
         <Renderer :content="input" />
       </div>
@@ -13,49 +18,49 @@
   </div>
 </template>
 <script>
-  import cookies from "js-cookie";
-  export default {
-    head() {
-      return {
-        title: "Create Tutorial"
-      }
+import cookies from "js-cookie";
+export default {
+  head() {
+    return {
+      title: "Create Tutorial",
+    };
+  },
+  data() {
+    return {
+      input: "",
+      title: "",
+      loading: false,
+    };
+  },
+  methods: {
+    updateTitle(e) {
+      this.title = e.target.value;
     },
-    data() {
-      return {
-        input: "",
-        title: "",
-        loading: false
-      }
+    update(e) {
+      this.input = e.target.value;
     },
-    methods: {
-      updateTitle(e) {
-        this.title = e.target.value
-      },
-      update(e) {
-        this.input = e.target.value
-      },
-      async createTutorial() {
-        this.loading = true
-        let res = await fetch(`${process.env.backendURL}/tutorial/new`, {
-          headers: {
-            "Content-Type": "application/json"
-          },
-          method: "PUT",
-          body: JSON.stringify({
-            body: this.input,
-            title: this.title
-          }),
-          credentials: "include"
-        })
-        
-        this.loading = false
-        let json = await res.json()
-        
-        if (json.id) return this.$router.push({ path: `/tutorials/${json.id}` })
-      }
+    async createTutorial() {
+      this.loading = true;
+      let res = await fetch(`${process.env.backendURL}/tutorial/new`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "PUT",
+        body: JSON.stringify({
+          body: this.input,
+          title: this.title,
+        }),
+        credentials: "include",
+      });
+
+      this.loading = false;
+      let json = await res.json();
+
+      if (json.id) return this.$router.push({ path: `/tutorials/${json.id}` });
     },
-    mounted() {
-      if (!cookies.get('auth')) this.$router.push({ path: "/login" })
-    }
-  }
+  },
+  mounted() {
+    if (!cookies.get("auth")) this.$router.push({ path: "/login" });
+  },
+};
 </script>
