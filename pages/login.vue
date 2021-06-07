1ef430c08b30c54b7d5daed628ba4f07c36c6b3f
FunctionalMetatable <FunctionalMetatable@users.noreply.github.com>
2021-06-07 16:58:43 +0000

    format code

--- pages/login.vue
+++ pages/login.vue
@@ -21,7 +21,7 @@ export default {
   data() {
     return {
       link: null,
-      error: (!!this.$route.query.error) || false
+      error: !!this.$route.query.error || false,
     };
   },
   mounted() {
