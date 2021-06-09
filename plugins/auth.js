47a3b463d63d81d14f00807ea4edf40c2c0790b3
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-09 16:13:08 +0100

    plugin

---
+++ plugins/auth.js
@@ -0,0 +1,13 @@
+export default ({ store }, inject) => {
+  inject('auth', {
+    isLoggedIn() {
+      return !!store.state.auth.user
+    },
+    user() {
+      return store.state.auth.user
+    },
+    token() {
+      return store.state.auth.token
+    }
+  })
+}
