767820dcee256e36e4dfc7340dcbb02137fe766e
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-10 16:21:34 +0100

    Create index.vue

---
+++ pages/test/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Renderer :content="data" />
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        data: `
+        \`\`\`scratchblocks
+        when green flag clicked
+        \`\`\`
+        `
+      }
+    }
+  }
+</script>
