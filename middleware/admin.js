fef4a77736b3e834651be970b8dcbd21223e2b08
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-10 07:43:54 +0100

    Create admin.js

---
+++ middleware/admin.js
@@ -0,0 +1,19 @@
+const cookie = process.server ? require("cookie") : require("js-cookie");
+
+export default async function ({ redirect, req, store }) {
+  let token = null;
+
+  if (process.server) {
+    if (req.headers.cookie) {
+      let token = cookie.parse(req.headers.cookie);
+    }
+  } else {
+    token = cookie.get("auth");
+  }
+
+  await store.dispatch('auth/refreshUserDetails', token)
+  
+  if (!$auth.user().admin) {
+    redirect('/')
+  }
+}
