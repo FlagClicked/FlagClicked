047452d15e5dede15fa0e08f07032471714028a6
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-11 20:07:11 +0100

    aaa

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="container">
-    <h3>loading</h3>
+    <div class="spinner"></div>
   </div>
 </template>
 <script>
 export default {};
 </script>
+<style scoped>
+  .spinner {
+    animation: spin 4s infinite;
+  }
+  @keyframes spin {
+    from { transform: rotate(0); }
+    to { transform: rotate(360); }
+  }
+</style>
