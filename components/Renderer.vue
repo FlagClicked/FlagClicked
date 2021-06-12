af640e6528ce02775e392fb7412fac49bdbbba63
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-12 17:24:35 +0100

    aaa

--- components/Renderer.vue
+++ components/Renderer.vue
@@ -4,62 +4,51 @@
 <script>
 import * as marked from "marked";
 import * as scratchblocks from "scratchblocks";
-
+import * as linkedom from "linkedom";
 export default {
   props: ["content"],
   data() {
-    return {};
+    return {
+      renderedContent: ""
+    };
   },
-  computed: {
-    renderedContent() {
-      let parseHTML;
-      if (process.server) return;
-      // from https://github.com/jeffalo/ocular/blob/main/components/Render.vue
-      let _document = parseHTML(
-        `<html><body>${marked(this.content)}</body></html>`
-      );
-
-      let options = {
-        style: "scratch3",
-        inline: false,
-        languages: ["en"],
-
-        read: scratchblocks.read,
-        parse: scratchblocks.parse,
-        render: scratchblocks.render,
-      };
-
-      let results = Array.from(
-        _document.querySelectorAll("code.lang-scratchblocks")
-      );
-
-      results.forEach((scratchblocks) => {
-        let code = options.read(scratchblocks, options);
-        let parsed = options.parse(code, options);
-        let svg = options.render(parsed, options);
-
-        let container = _document.createElement("div");
-        container.classList.add("scratchblocks");
-
-        container.appendChild(svg);
-
-        scratchblocks.innerHTML = "";
-
-        scratchblocks.appendChild(container);
-      });
-
-      let codeblocks = Array.from(
-        document.querySelectorAll("code:not(.lang-scratchblocks)")
-      );
-
-      codeblocks.forEach((el) => {
-        el.setAttribute("data-language", el.classList[0]?.slice(5));
-        Rainbow.color(el.parentNode);
-      });
-
-      return _document.querySelector("body").innerHTML;
-    },
-  },
+  mounted() {
+    let doc = (new window.DOMParser()).parseFromString(`<html><body>${this.content}</body></html>`, "text/html")
+    
+    let sbOptions = {
+      style: "scratch3",
+      inline: false,
+      languages: ["en"],
+      read: scratchblocks.read,
+      parse: scratchblocks.parse,
+      render: scratchblocks.render
+    }
+    
+    let sb = Array.from(doc.querySelectorAll('code.lang-scratchblocks'))
+    
+    sb.forEach(blocks => {
+      let code = options.parse(blocks, options)
+      let parsed = options.parse(code, options)
+      let svg = options.render(parsed, options)
+      
+      let container = doc.createElement('div')
+      
+      container.appendChild(svg)
+      
+      blocks.innerHTML = '';
+      
+      blocks.appendChild(container)
+    })
+    
+    let codeblocks = Array.from(doc.querySelectorAll("code:not(.lang-scratchblocks)"))
+    
+    codeblocks.forEach(el => {
+      el.setAttribute('data-language', el.classList[0]?.slice(5))
+      // Rainbow.color(el.parentNode) // <<< TODO
+    })
+    
+    this.renderedContent = doc.querySelector('body').innerHTML
+  }
   ssr: false,
 };
 </script>
